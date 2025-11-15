91c339393e3a7db1642386b1ebe06263ad89d37f
gMBergamo <giovannibergamo88@gmail.com>
2025-11-15 18:14:59 -0300

    Comentários na classe User e explanação da atividade no README.md

--- README.md
+++ README.md
@@ -1,18 +1,53 @@
-## Getting Started
+## Resumo
+Abaixo estão as atividades solicitadas pelo professor Daniel Ohata na matéria AS406TSN2 - UX/UI e Testes de Software.
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Aluno: Giovanni M. G. Bergamo - RA: 248745.
 
-## Folder Structure
+## Identificação dos nós
 
-The workspace contains two folders by default, where:
+1. public boolean verificarUsuario(String login, String senha){
+    
+2. String sql = "";<br>
+    Connection conn = conectarBD();
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+3. sql += "select nome from usuarios ";<br>
+    sql +="where login = " + "'" + login + "'";<br>
+    sql += " and senha = " + "'" + senha + "';";
+    
+4. try {
+    Statement st = conn.createStatement();<br>
+    ResultSet rs = st.executeQuery(sql);<br>
+5. if(rs.next()){
+6. nome = rs.getString("nome");<br>
+            result = true;}
+} 
+7. catch (Exception e) { } 
+8. return result;
+}
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+## Notação de grafo de fluxo
 
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+       1 -> 2 -> 3 -> 4 --> 5 -> 6--\ 
+                        \    \ -> ----> 8 (Fim)
+                         \ -> 7 ------/
 
-## Dependency Management
+## Complexidade Ciclomática
 
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Formula: M = E − N + 2P
+
+- E, é o número de arestas, neste programa 8.
+- N, é o numero de nós, neste programa 8.
+- P, é o número de componentes conectados.
+
+Sendo assim: M = 8 - 8 + 2(1) -> M = 2
+
+A complexidade ciclomática deste programa é 2.
+
+## Caminhos básicos
+- Retorno positivo:<br>
+1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 8 (Usuário encontrado)
+- Retorno negativo:<br>
+1 -> 2 -> 3 -> 4 -> 5 -> 8 (Usuário não encontrado)<br>
+1 -> 2 -> 3 -> 4 -> 7 -> 8 (Exeção)
+
+Apesar de existirem 3 caminhos, podemos considerar como apenas 2 caminhos lógicos, já que apenas o caminho 1 retorna true e os caminhos 2 e 3 retornam false, passando a atender ao resultado da complexidade ciclomática.
--- src/login/User.java
+++ src/login/User.java
@@ -6,29 +6,44 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class User {
+
+    // Método para conectar ao Banco de Dados
+    // TODO: Realizar a conexão com o banco de dados em uma classe separada.
     public Connection conectarBD(){
         Connection conn = null;
         try {
+            // Carregar o driver JDBC do MySQL
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            // Estabelecer a conexão com o banco de dados
+            // TODO: ajustar URL, usuário e senha conforme o seu ambiente, tranferindo-os para variáveis de ambiente em uma classe separada.
+            // OBS: código abaixo está com credenciais escritas no código, não sendo possível executá-lo em qualquer ambiente e esperar que haja algum retorno no método verificarUsuario.
             conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/test?user=lopes&password=123");
-        } catch (Exception e) { }
+        } catch (Exception e) { } // TODO: tratar exceção adequadamente, desta forma não é possível identificar erros de conexão.
         return conn; }
     public String nome="";
     public boolean result = false;
+
+    // Método para verificar usuário no Banco de Dados
     public boolean verificarUsuario(String login, String senha){
+        // TODO: Tratar possíveis exceções e null pointers para login, senha e conn.
+        // TODO: Ajustar indentação e formatar código.
         String sql = "";
         Connection conn = conectarBD();
+        // OBS: Devido a conexão estar com credenciais escritas no código, não será possível executar este método e esperar que haja algum retorno diferente de false.
         // INSTRUÇÃO SQL
         sql += "select nome from usuarios ";
         sql +="where login = " + "'" + login + "'";
         sql += " and senha = " + "'" + senha + "';";
+        // OBS: Como não existem verificações para os valores recebidos em login e senha, este código está vulnerável a ataques de SQL Injection. Poderá usar Prepared Statements para evitar este tipo de vulnerabilidade.
+        // Executar a consulta SQL
         try {
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(sql);
             if(rs.next()){
                 nome = rs.getString("nome");
                 result = true;}
-        } catch (Exception e) { }
+        } catch (Exception e) { } // TODO: tratar exceção adequadamente, desta forma não é possível identificar erros na execução da query.
+        // TODO: Fechar conexões com banco de dados, statements e result sets. Se não fechados, podem ocorrer vazamentos de memória e esgotamento de conexões.
         return result;
     }// fim da class
 }
